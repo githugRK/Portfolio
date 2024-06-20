044a5824e0f3447d63fc98515becac8a169d0659
Rama Krishna <103884422+githugRK@users.noreply.github.com>
2024-06-20 16:17:45 +0530

    Updated

--- main.js
+++ main.js
@@ -6,3 +6,34 @@ var typed = new Typed(".text",{
     backDelay : 1000,
     loop : true
 });
+
+// Get the modal and the image element within it
+var modal = document.getElementById("certificateModal");
+var modalImg = document.getElementById("certificateImage");
+
+// Get all "View Certificate" buttons
+var btns = document.querySelectorAll(".view-certificate");
+
+// Attach click event listeners to each button
+btns.forEach(function(btn) {
+  btn.onclick = function() {
+    var imgSrc = this.getAttribute("data-img-src"); // Get the image source from data-img-src attribute
+    modalImg.src = imgSrc; // Set the modal image source
+    modal.style.display = "block"; // Display the modal
+  }
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
